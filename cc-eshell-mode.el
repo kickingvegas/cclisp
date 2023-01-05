a7653ea028d6069614a0985f44e684ac45ce0eae
Charles Choi <kickingvegas@gmail.com>
2023-01-05 10:41:58 -0800

    Initial Entry

---
+++ cc-eshell-mode.el
@@ -0,0 +1,15 @@
+;; eshell-mode
+
+(setq eshell-prompt-regexp "┗━━ \\$ "
+      eshell-prompt-function
+      (lambda nil
+        (concat "\n┏━ "
+                (user-login-name) "@" (system-name) ":"
+                (propertize (if (string= (eshell/pwd) (getenv "HOME"))
+                                "~"
+                              (replace-regexp-in-string
+                               (concat "^" (getenv "HOME")) "~" (eshell/pwd)))
+                            'face `(:foreground "blue"))
+                "\n┗━━ "
+                (if (= (user-uid) 0) "# " "$ "))))
+
