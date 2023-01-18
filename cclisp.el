7dfa166d2731f568c8989c011f16944f5c81fb99
Charles Choi <kickingvegas@gmail.com>
2023-01-17 18:17:33 -0800

    Fix Search with Google context menu

--- cclisp.el
+++ cclisp.el
@@ -388,7 +388,7 @@ A new frame will be created if pop-up-frames is t"
 
     (when (region-active-p)
       (define-key-after menu [google-search]
-        '(menu-item "Search with Google" google-this-noconfirm
+        '(menu-item (cc/context-menu-label "Search with Google") google-this-noconfirm
                     :help "Search Google with region"))
       (define-key-after menu [webpaste-paste-region]
         '(menu-item (cc/context-menu-label "Webpaste") webpaste-paste-region
