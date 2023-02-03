22e228904ab095702a5242ab763e73a332ece641
Charles Choi <kickingvegas@gmail.com>
2023-02-03 14:59:39 -0800

    Initial Entry

---
+++ cc-google-translate.el
@@ -0,0 +1,6 @@
+(require 'google-translate)
+(require 'google-translate-smooth-ui)
+;;(global-set-key "\C-ct" 'google-translate-smooth-translate)
+
+(setq google-translate-translation-directions-alist
+      '(("ko" . "en") ("en" . "ko")))
