9745387da59cc8d444be415c127053b1c4c8039f
Charles Choi <kickingvegas@gmail.com>
2023-05-23 11:51:40 -0700

    Initial Entry

---
+++ cc-repeat-mode.el
@@ -0,0 +1,30 @@
+;; repeat-mode
+
+(defvar vifon/buffer-nextprev-repeat-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<up>")    #'next-buffer)
+    (define-key map (kbd "<right>") #'next-buffer)
+    (define-key map (kbd "<down>")  #'previous-buffer)
+    (define-key map (kbd "<left>")  #'previous-buffer)
+    map))
+;;(put 'next-buffer     'repeat-map 'vifon/buffer-nextprev-repeat-map)
+;;(put 'previous-buffer 'repeat-map 'vifon/buffer-nextprev-repeat-map)
+
+(defun repeatize (keymap)
+  "Add `repeat-mode' support to a KEYMAP."
+  (map-keymap
+   (lambda (_key cmd)
+     (when (symbolp cmd)
+       (put cmd 'repeat-map keymap)))
+   (symbol-value keymap)))
+
+(defvar cc/org-header-navigation-repeat-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "p")    #'org-previous-visible-heading)
+    (define-key map (kbd "n")  #'org-next-visible-heading)
+    map))
+
+(repeatize 'vifon/buffer-nextprev-repeat-map)
+(repeatize 'cc/org-header-navigation-repeat-map)
+
+(repeat-mode)
