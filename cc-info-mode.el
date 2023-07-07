6c599fb873b96f8e2d9795bb9272c89fc1676797
Charles Choi <kickingvegas@gmail.com>
2023-07-06 21:57:15 -0700

    Initial Entry

---
+++ cc-info-mode.el
@@ -0,0 +1,6 @@
+
+(add-hook 'Info-mode-hook (lambda ()
+                            (define-key Info-mode-map (kbd "M-[") 'Info-history-back)
+                            (define-key Info-mode-map (kbd "M-]") 'Info-history-forward)
+                            (define-key Info-mode-map (kbd "<mouse-5>") 'Info-history-forward)
+                            (define-key Info-mode-map (kbd "<mouse-4>") 'Info-history-back)))
