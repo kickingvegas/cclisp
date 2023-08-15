ae109b390e058aa624696656a9615d3a36be4018
Charles Choi <kickingvegas@gmail.com>
2023-08-14 18:16:43 -0700

    Updated info directory

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Info-additional-directory-list '("/opt/local/share/info"))
  '(Info-scroll-prefer-subnodes t)
  '(Man-notify-method 'aggressive)
  '(apropos-do-all t)
