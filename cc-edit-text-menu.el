2f13df93a743f03f5da18dc0fcf9851b4270af2e
Charles Choi <kickingvegas@gmail.com>
2023-11-14 18:39:16 -0800

    Added more delete menu items
    - whitespace-cleanup
    - delete-trailing-whitespace

--- cc-edit-text-menu.el
+++ cc-edit-text-menu.el
@@ -1,7 +1,7 @@
 ;;; cc-edit-text-menu.el --- Edit Text Menus
 
 ;;; Commentary:
-;; 
+;;
 
 (easy-menu-define cc/transpose-menu nil
   "Keymap for Transpose submenu"
@@ -9,16 +9,16 @@
     :visible (not buffer-read-only)
     ["Characters" transpose-chars
      :help "Interchange characters around point, moving forward one character."]
-    
+
     ["Words" transpose-words
      :help "Interchange words around point, leaving point at end of them."]
-     
+
     ["Lines" transpose-lines
      :help "Exchange current line and previous line, leaving point after both."]
-  
+
     ["Sentences" transpose-sentences
      :help "Interchange the current sentence with the next one."]
-     
+
     ["Paragraphs" transpose-paragraphs
      :help "Interchange the current paragraph with the next one."]
 
@@ -35,20 +35,20 @@ expressions (sexps)."]))
     :visible (not buffer-read-only)
     ["Word Forward" cc/move-word-forward
      :help "Move word to the right of point forward one word."]
-    
+
     ["Word Backward" cc/move-word-backward
      :help "Move word to the right of point backward one word."]
 
     ["Sentence Forward" cc/move-sentence-forward
      :help "Move sentence to the right of point forward one sentence."]
-    
+
     ["Sentence Backward" cc/move-sentence-backward
      :help "Move sentence to the right of point backward one sentence."]
 
     ["Balanced Expression (sexp) Forward" cc/move-sexp-forward
      :help "Move balanced expression (sexp) to the right of point forward \
 one sexp."]
-    
+
     ["Balanced Expression (sexp) Backward" cc/move-sexp-backward
      :help "Move balanced expression (sexp) to the right of point backward \
 one sexp."]))
@@ -60,17 +60,23 @@ one sexp."]))
     ["Join Line" join-line
      :help "Join this line to previous and fix up \
 whitespace at join"]
-    
+
     ["Just One Space" just-one-space
      :help "Delete all spaces and tabs around point, leaving \
 one space."]
 
     ["Delete Horizontal Space" delete-horizontal-space
      :help "Delete all spaces and tabs around point."]
-    
+
     ["Delete Blank Lines" delete-blank-lines
      :help "On blank line, delete all surrounding blank lines, \
-leaving just one."]))
+leaving just one."]
+
+    ["Whitespace Cleanup" whitespace-cleanup
+     :help "Cleanup some blank problems in all buffer or at region."]
+
+    ["Delete Trailing Whitespace" delete-trailing-whitespace
+     :help "Delete trailing whitespace between START and END."]))
 
 
 (provide 'cc-edit-text-menu)
