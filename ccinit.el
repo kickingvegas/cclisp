cb7d415ecdd3b92361f61cb132e9c70e6034937a
Charles Choi <kickingvegas@gmail.com>
2023-11-14 23:20:55 -0800

    Changes to support pixel-scroll-precision-mode.

--- ccinit.el
+++ ccinit.el
@@ -24,9 +24,10 @@
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse 't)
-(setq scroll-step 1)
+;(setq scroll-step 1)
 (setq mac-mouse-wheel-mode t)
-(setq mac-mouse-wheel-smooth-scroll t)
+(setq mac-mouse-wheel-smooth-scroll nil)
+(pixel-scroll-precision-mode 1)
 
 (require 'avy)
 (require 'cclisp)
