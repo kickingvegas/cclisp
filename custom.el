5ef58f7eabfb95a88d244e4b5ce37c101035e675
Charles Choi <kickingvegas@gmail.com>
2023-12-03 23:34:37 -0800

    Tune font-lock-comment-face for tty

--- custom.el
+++ custom.el
@@ -425,7 +425,7 @@
  '(diff-added ((t (:inherit diff-changed :extend t :background "lawn green"))))
  '(eshell-prompt ((t (:foreground "#cc33ff" :weight bold))))
  '(fixed-pitch ((t (:family "Menlo" :height 150))))
- '(font-lock-comment-face ((((type x mac ns) (class color) (background light)) (:foreground "dim gray")) (((type x mac ns) (class color) (background dark)) (:foreground "dark gray")) (((type tty) (class color)) (:foreground "dark gray"))))
+ '(font-lock-comment-face ((((type x mac ns) (class color) (background light)) (:foreground "dim gray")) (((type x mac ns) (class color) (background dark)) (:foreground "dark gray")) (((type tty) (class color)) (:foreground "medium turquoise"))))
  '(font-lock-constant-face ((t (:height 1.0))))
  '(font-lock-function-name-face ((((type x mac ns) (class color) (background light)) (:foreground "Blue1")) (((type x mac ns) (class color) (background dark)) (:foreground "LightSkyBlue")) (((type tty) (class color)) (:foreground "yellow")) (t (:weight bold))))
  '(highlight ((((type x mac ns) (class color) (background light)) (:background "darkseagreen2")) (((type x mac ns) (class color) (background dark)) (:background "cyan" :foreground "black")) (((type tty) (class color)) (:background "green" :foreground "black")) (t (:inverse-video t))))
