19f7311d9ab78efc9aefd733039c4a8187b60d56
Charles Choi <kickingvegas@gmail.com>
2024-02-15 14:21:49 -0800

    Turn on flycheck in emacs-lisp-mode.

--- cc-emacs-lisp-mode.el
+++ cc-emacs-lisp-mode.el
@@ -24,10 +24,13 @@
 
 (require 'elisp-mode)
 (require 'paredit)
+(require 'flycheck)
 
 ;;; Code:
 
 (add-hook 'emacs-lisp-mode 'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode 'flycheck-mode)
+
 
 (define-key emacs-lisp-mode-map (kbd "M-[") 'backward-sexp)
 (define-key emacs-lisp-mode-map (kbd "M-]") 'forward-sexp)
