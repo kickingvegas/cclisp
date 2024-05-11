31a9b6f7b0a8f1ccd8871de4b841bb6b1f65e494
Charles Choi <kickingvegas@gmail.com>
2024-05-10 22:00:02 -0700

    Support richer window management, add/tune support for modes
    - Support windmove* commands
    - Added Occur mode config
    - Added Python mode config
    - Added Swift mode config
    - Tuned configuration for sunrise-sunset

---
+++ cc-swift-mode.el
@@ -0,0 +1,33 @@
+;;; cc-swift-mode.el --- Swift mode configuration    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Charles Choi
+
+;; Author: Charles Choi <kickingvegas@gmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'swift-mode)
+
+(define-key swift-mode-map (kbd "M-[") #'backward-sexp)
+(define-key swift-mode-map (kbd "M-]") #'forward-sexp)
+
+(provide 'cc-swift-mode)
+;;; cc-swift-mode.el ends here
