905ebaad765350c5d745a25856ed97f6f1d14e84
Charles Choi <kickingvegas@gmail.com>
2024-09-17 12:54:19 -0700

    Fix F10 binding

--- cc-global-keybindings.el
+++ cc-global-keybindings.el
@@ -67,7 +67,7 @@
 (keymap-global-set "M-<f7>" #'repeat-complex-command)
 (keymap-global-set "C-o" #'casual-editkit-main-tmenu)
 (if (string-equal (window-system) "mac")
-    (keymap-global-set "<f10>" #'cc/main-tmenu))
+    (keymap-global-set "<f10>" #'casual-editkit-main-tmenu))
 
 (keymap-global-set "<f8>" #'org-capture)
 (keymap-global-set "<f9>" #'compile)
