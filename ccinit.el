a0540eaee319a4433bfbb9decb620004d1d6ee4e
Charles Choi <kickingvegas@gmail.com>
2024-12-09 10:59:20 -0800

    Add ImageMagick7 to path.

--- ccinit.el
+++ ccinit.el
@@ -28,9 +28,11 @@
 (when (or (eq window-system 'mac) (eq window-system 'ns))
   (setenv "PATH" (concat "/opt/local/bin:" (getenv "PATH")))
   (setenv "PATH" (concat "/opt/local/libexec/gnubin:" (getenv "PATH")))
+  (setenv "PATH" (concat "/opt/local/lib/ImageMagick7/bin" (getenv "PATH")))
   (setenv "PATH" (concat (getenv "HOME") "/bin:" (getenv "PATH")))
   (add-to-list 'exec-path "/opt/local/bin")
   (add-to-list 'exec-path "/opt/local/libexec/gnubin")
+  (add-to-list 'exec-path "/opt/local/lib/ImageMagick7/bin")
   (add-to-list 'exec-path (concat (getenv "HOME") "/bin")))
 
 (require 'use-package)
