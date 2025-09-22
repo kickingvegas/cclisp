11e00892114de2ffa552df9253cea65751bb4daa
Charles Choi <kickingvegas@gmail.com>
2025-09-22 15:38:18 -0700

    Support Casual BibTeX

--- cc-bibtex-mode.el
+++ cc-bibtex-mode.el
@@ -22,81 +22,10 @@
 
 ;;
 
-
 ;;; Code:
 (require 'bibtex)
-(require 'replace)
 (require 'hl-line)
-(require 'bookmark)
-(require 'casual-lib)
-
-(defun cc/bibtex-previous-field ()
-  (interactive)
-  (forward-line -2)
-  (bibtex-next-field nil))
-
-(defun cc/bibtex-fill-and-clean (&optional new-key called-by-reformat)
-  "Fill and clean BibTeX entry with options NEW-KEY, CALLED-BY-REFORMAT."
-  (interactive "P")
-  (bibtex-fill-entry)
-  (bibtex-clean-entry new-key called-by-reformat))
-
-(transient-define-prefix casual-bibtex-tmenu ()
-  "Transient menu for bibtex."
-  :refresh-suffixes t
-
-  ["Casual BibTeX"
-   ["Field"
-    :pad-keys t
-    ("a" "Add…" bibtex-make-field :transient nil)
-    ("c" "Copy" bibtex-copy-field-as-kill :transient nil)
-    ("x" "Clear" bibtex-empty-field :transient nil)
-    ("DEL" "Delete" bibtex-kill-field :transient nil)
-    ("o" "Remove OPT/ALT" bibtex-remove-OPT-or-ALT :transient nil)]
-
-   ["Entry"
-    :pad-keys t
-    ("A" "Add…" bibtex-entry :transient nil)
-    ("C" "Copy" bibtex-copy-entry-as-kill :transient nil)
-    ("k" "Kill" bibtex-kill-entry :transient nil)
-    ("u" "Update" bibtex-entry-update :transient nil)
-    ("m" "Mark" bibtex-mark-entry :transient nil)]
-
-   ["Yank/Fill/Clean"
-    :pad-keys t
-    ("y" "Yank" bibtex-yank :transient t)
-    ("M-y" "Yank-Pop" bibtex-yank-pop :transient t)
-    ("f" "Fill" bibtex-fill-entry :transient nil)
-    ("C-c" "Fill & Clean" cc/bibtex-fill-and-clean :transient nil)]
-
-   ["Navigation"
-    :pad-keys t
-    ("p" "Previous Field" previous-line :transient t)
-    ("n" "Next Field" bibtex-next-field :transient t)
-    ("M-p" "Previous Entry" bibtex-previous-entry :transient t)
-    ("M-n" "Next Entry" bibtex-next-entry :transient t)
-    ("<" "Begin Entry" bibtex-beginning-of-entry :transient t)
-    (">" "End Entry" bibtex-end-of-entry :transient t)]]
-
-  ["Misc"
-   [("/" "Search…" bibtex-search-entries :transient t)
-    ("j" "Jump…" bibtex-search-entry :transient t)]
-   [("." "Xref…" bibtex-search-crossref :transient t)
-    ("s" "Sort" bibtex-sort-buffer :transient t)]
-   [("O" "Occur…" occur)
-    ("N" "Narrow" bibtex-narrow-to-entry
-     :if-not buffer-narrowed-p
-     :transient nil)
-    ("W" "Widen" widen
-     :if buffer-narrowed-p
-     :transient nil)]
-   [("J" "Jump to Bookmark…" bookmark-jump)]]
-
-  [:class transient-row
-          (casual-lib-quit-one)
-          ("RET" "Edit" transient-quit-all)
-          ("U" "Undo" undo :transient t)
-          (casual-lib-quit-all)])
+(require 'casual-bibtex)
 
 (add-hook 'bibtex-mode-hook 'hl-line-mode)
 
@@ -112,7 +41,7 @@
 (keymap-set bibtex-mode-map "<next>" #'bibtex-next-entry)
 
 (keymap-set bibtex-mode-map "C-c C-o" #'bibtex-url)
-(keymap-set bibtex-mode-map "C-c C-c" #'cc/bibtex-fill-and-clean)
+(keymap-set bibtex-mode-map "C-c C-c" #'casual-bibtex-fill-and-clean)
 
 (keymap-set bibtex-mode-map "<clear>" #'bibtex-empty-field)
 (keymap-set bibtex-mode-map "M-<clear>" #'bibtex-kill-field)
