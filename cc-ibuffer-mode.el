a51abe0c5fd6fa998d4192f883fdabeeae5744b2
Charles Choi <kickingvegas@gmail.com>
2025-09-26 15:21:43 -0700

    Remove commented code
    * cc-ibuffer-mode.el: remove commented code

--- cc-ibuffer-mode.el
+++ cc-ibuffer-mode.el
@@ -23,25 +23,6 @@
 ;;
 
 ;;; Code:
-;; (use-package ibuffer
-;;   :hook (ibuffer-mode . ibuffer-auto-mode)
-;;   :defer t)
-
-;; (use-package casual-ibuffer
-;;   :ensure nil
-;;   :bind (:map
-;;          ibuffer-mode-map
-;;          ("C-o" . casual-ibuffer-tmenu)
-;;          ("F" . casual-ibuffer-filter-tmenu)
-;;          ("s" . casual-ibuffer-sortby-tmenu)
-;;          ("<double-mouse-1>" . ibuffer-visit-buffer)
-;;          ("M-<double-mouse-1>" . ibuffer-visit-buffer-other-window)
-;;          ("{" . ibuffer-backwards-next-marked)
-;;          ("}" . ibuffer-forward-next-marked)
-;;          ("[" . ibuffer-backward-filter-group)
-;;          ("]" . ibuffer-forward-filter-group)
-;;          ("$" . ibuffer-toggle-filter-group))
-;;   :after (ibuffer))
 
 (require 'ibuffer)
 (require 'ibuf-ext)
@@ -56,8 +37,10 @@
 (keymap-set ibuffer-name-map "<double-mouse-1>" #'ibuffer-mouse-visit-buffer)
 (keymap-set ibuffer-name-map "<mouse-2>" #'ibuffer-mouse-toggle-mark)
 
-(keymap-set ibuffer-mode-filter-group-map "<mouse-1>" #'ibuffer-mouse-toggle-filter-group)
-(keymap-set ibuffer-mode-filter-group-map "<mouse-2>" #'ibuffer-mouse-toggle-mark)
+(keymap-set ibuffer-mode-filter-group-map
+            "<mouse-1>" #'ibuffer-mouse-toggle-filter-group)
+(keymap-set ibuffer-mode-filter-group-map
+            "<mouse-2>" #'ibuffer-mouse-toggle-mark)
 
 (keymap-set ibuffer-mode-map "<f1>" #'ibuffer-jump-to-filter-group)
 (keymap-set ibuffer-mode-map "<f2>" #'ibuffer-jump-to-buffer)
@@ -72,7 +55,6 @@
 (keymap-set ibuffer-mode-map "]" #'ibuffer-forward-filter-group)
 (keymap-set ibuffer-mode-map "$" #'ibuffer-toggle-filter-group)
 (keymap-set ibuffer-mode-map "J" #'ibuffer-jump-to-filter-group)
-
 
 (provide 'cc-ibuffer-mode)
 ;;; cc-ibuffer-mode.el ends here
