b11363fd41c072cd2641f66124a44d20f62fc0c2
Charles Choi <kickingvegas@gmail.com>
2025-12-18 14:20:12 -0800

    Fix changes to package dependencies
    - Changes to diff-hl, git-link now need direct install.

--- custom.el
+++ custom.el
@@ -462,17 +462,18 @@
  '(package-install-upgrade-built-in t)
  '(package-selected-packages
    '(async auto-complete bind-key calfw calle24 citeproc company company-org-block
-           countdown csv-mode debbugs dired-rsync-transient disk-usage ebib
-           edit-indirect editorconfig eglot eldoc elfeed erc eshell-git-prompt
-           expand-region faceup fireplace flycheck-package gnuplot go-mode
-           good-scroll google-this google-translate graphviz-dot-mode
-           helm-bibtex helm-pass helm-swoop htmlize idlwave iedit js2-mode
-           json-mode jsonian jsonrpc kanban ledger-mode magit math-symbol-lists
-           neotree numeri ob-swift ob-swiftui org-outline-numbering org-ql
-           org-re-reveal org-superstar osx-dictionary ox-gfm ox-gist ox-jira
-           ox-slack ox-trac package-lint paredit password-store pbcopy pkg-info
-           plantuml-mode project python rainbow-mode reveal-in-folder scpaste
-           show-font smart-mode-line-powerline-theme soap-client solarized-theme
+           countdown csv-mode debbugs diff-hl dired-rsync-transient disk-usage
+           ebib edit-indirect editorconfig eglot eldoc elfeed erc
+           eshell-git-prompt expand-region faceup fireplace flycheck-package
+           git-link gnuplot go-mode good-scroll google-this google-translate
+           graphviz-dot-mode helm-bibtex helm-pass helm-swoop htmlize idlwave
+           iedit js2-mode json-mode jsonian jsonrpc kanban ledger-mode magit
+           math-symbol-lists neotree numeri ob-swift ob-swiftui
+           org-outline-numbering org-ql org-re-reveal org-superstar
+           osx-dictionary ox-gfm ox-gist ox-jira ox-slack ox-trac package-lint
+           paredit password-store pbcopy pkg-info plantuml-mode project python
+           rainbow-mode reveal-in-folder scpaste show-font
+           smart-mode-line-powerline-theme soap-client solarized-theme
            sqlite-mode-extras sr-speedbar svg-clock swift-mode symbol-overlay
            tj3-mode toc-org track-changes transpose-frame treemacs use-package
            visual-regexp visual-regexp-steroids vtable webpaste wgrep which-key
